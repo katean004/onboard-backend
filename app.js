a31c173edf05c0ad1e5bc7fafe27f71bbe3cfc90
kate <kan004@ucr.edu>
2021-10-20 15:32:37 -0700

    changed db url

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ const app = express();
 //   next();
 // });
 
-// const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/users";
-const dbUrl = "mongodb://localhost:27017/users";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/users";
 
 mongoose.connect(dbUrl);
 
@@ -30,7 +29,7 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.render("home");
 });
 
