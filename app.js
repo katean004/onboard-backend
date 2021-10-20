7f18d70da88e0cac6974428fbc6721793baac2e8
kate <kan004@ucr.edu>
2021-10-20 15:44:34 -0700

    changed db url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 //   next();
 // });
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/users";
 
 mongoose.connect(dbUrl);
 
